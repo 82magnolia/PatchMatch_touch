6a2ea3e9038e6b03c58fce8f643de3093cec4920
82magnolia <82magnolia@snu.ac.kr>
2026-07-09 16:08:53 +0900

    Fixed run scripts for gpu ids

--- train_refine_scripts/gen_contact_ref_multi_pseudo_mini/run_gpu5.sh
+++ train_refine_scripts/gen_contact_ref_multi_pseudo_mini/run_gpu5.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-bash "$(dirname "${BASH_SOURCE[0]}")/_run.sh" "${1:?Usage: run_gpu5.sh <GPU_ID>}"
+bash "$(dirname "${BASH_SOURCE[0]}")/_run.sh" 5 "${1:?Usage: run_gpu5.sh <GPU_ID>}"
