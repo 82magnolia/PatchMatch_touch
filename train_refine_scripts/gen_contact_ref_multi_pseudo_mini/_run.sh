f5f281c8ed1d72f6012306381922210fe3d2454b
82magnolia <82magnolia@snu.ac.kr>
2026-07-09 06:02:29 +0000

    Fixed theta magnitudes

--- train_refine_scripts/gen_contact_ref_multi_pseudo_mini/_run.sh
+++ train_refine_scripts/gen_contact_ref_multi_pseudo_mini/_run.sh
@@ -69,8 +69,6 @@ for obj_dir in "$OBJ_DIR"/*/; do
         --contact_ply "$contact_ply" \
         --mode back_forth_press \
         --depth_range_info 0. 10. 50 \
-        --rand_contact_theta \
-        --rand_contact_theta_mag 0.5235987755982988 \
         --save_dir "$save_dir" \
         --obj_scale_factor 100.0 \
         --override_hw 240 320 \
